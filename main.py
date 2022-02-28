d4a06b86ee3d216d23fb2d7164a7e69df23ebcb6
Parya2001 <92047309+Parya2001@users.noreply.github.com>
2022-02-28 19:28:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
-req = requests.get("https://vedabase.io/en/library/bg/1/1/")
-soup: BeautifulSoup = BeautifulSoup(req.content,'html.parser')
+def getdata(url):
+    r = requests.get(url )
+    return r.text
 
-sanskrit_verse        = []
-english_text          = []
-word_to_word_meaning  = []
-trans_lation          = []
-pur_port              = []
+url = "https://vedabase.io/en/library/bg/1/1/"
 
-for p in soup.find_all('div', attrs={'class':'col-12'}):
-    verse = p.find('div', attrs ={'class':'wrapper-devanagari'})
-    print(verse)
-    text = p.find('div', attrs ={'class':'wrapper-verse-text'})
-    print(text)
-    meaning = p.find('div', attrs ={'class':'wrapper-synonyms'})
-    print(meaning)
-    trans_ = p.find('div', attrs ={'class':'wrapper-translation'})
-    print(trans_)
-    pur_ = p.find('div', attrs ={'class':'wrapper-puport'})
-    print(pur_)
-
-sanskrit_verse.append(verse)
-english_text.append(text)
-word_to_word_meaning.append(meaning)
-trans_lation.append(trans_)
-pur_port.append(pur_)
-
-print(sanskrit_verse)
-print(english_text)
-print(word_to_word_meaning)
-print(trans_lation)
-print(pur_port)
-
-
+htmldata = getdata(url)
+soup = BeautifulSoup(htmldata, 'html.parser')
+#print(soup)
+data_str = ""
+for item in soup.find_all("div", class_="wrapper-devanagari"):
+    data_str = data_str + item.get_text()
+    for item in soup.find_all("div", class_="wrapper-verse-text"):
+         data_str = data_str + item.get_text()
+    for item in soup.find_all("div", class_="wrapper-synonyms"):
+         data_str = data_str + item.get_text()
+    for item in soup.find_all("div", class_="wrapper-translation"):
+         data_str = data_str + item.get_text()
+    for item in soup.find_all("div", class_="wrapper-puport"):
+         data_str = data_str + item.get_text()
+print(data_str)
 
 
 
