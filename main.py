30df883a5b187711ca3f1a87a6fe0d383d555346
Parya2001 <92047309+Parya2001@users.noreply.github.com>
2022-03-18 12:07:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,5 +22,13 @@ for item in soup.find_all("div", class_="wrapper-devanagari"):
          data_str = data_str + item.get_text()
 print(data_str)
 
-
-
+page = 1
+titles = []
+while page != 35:
+      url = f"https://vedabase.io/en/library/bg/1/1/?page={page}"
+      response = requests.get(url)
+      html = response.content
+      soup = bs(html, "lxml")
+      for h3 in soup.find_all("h2", class_="wrapper devnagari"):
+            titles.append(h3.get_text(strip=True))
+      page = page + 1
