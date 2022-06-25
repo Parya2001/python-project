9910c46f353d21d90510dcb571dea3208743111c
Parya2001 <92047309+Parya2001@users.noreply.github.com>
2022-06-25 17:28:52 +0530

    Update bg.py

--- bg.py
+++ bg.py
@@ -39,20 +39,7 @@ cred = credentials.Certificate("krishna.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
-
+    
 doc_ref = db.collection(u'Bhagavadgita As it is').document(u'chapter 1.1')
-doc_ref = db.document(u"धर्मक्षेत्रे कुरुक्षेत्रे समवेता युयुत्सव: ।मामका: पाण्डवाश्चैव किमकुर्वत सञ्जय ॥ १ ॥")
-#doc_ref.set({u"dhṛtarāṣṭra uvāca dharma-kṣetre kuru-kṣetre samavetā yuyutsavaḥ māmakāḥ pāṇḍavāś caiva kim akurvata sañjaya"})
-doc_ref.set({    
-    u'dhṛtarāṣṭraḥ uvāca':u'King Dhṛtarāṣṭra said', 
-    u'dharma-kṣetre':u'in the place of pilgrimage;',
-    u'kuru-kṣetre —':u'in the place named Kurukṣetra;',
-    u'samavetāḥ —':u'assembled;',
-    u'yuyutsavaḥ —':u'desiring to fight;',
-    u'māmakāḥ —':u'my party (sons);',
-    u'pāṇḍavāḥ —':u'the sons of Pāṇḍu;',
-    u'ca —':u'and;',
-    u'eva —':u'certainly;',
-    u'kim —':u'what;',
-    u'akurvata —':u'did they do;',
-    #u'sañjaya —': OSañjaya
+doc_ref = db.collection(u'sloka').document(u'verse'),(u'word to word'),(u'translation'),(u'purport')
+
